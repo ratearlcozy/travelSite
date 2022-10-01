c3458cf04afc36f51ca0b2cc9f039165885c824d
ratearlcozy <radepetrovic99@gmail.com>
2022-10-01 22:06:58 +0200

    login, signup window, finished home page, only mobile

--- index.js
+++ index.js
@@ -4,3 +4,31 @@ function toggleNav() {
     nav.style.display = "none" : 
     nav.style.display = "block";
 }
+
+function openFormLogin() {
+    let form = document.getElementById("loginForm");
+    form.style.display === "none" ?
+    form.style.display = "block":
+    form.style.display = "none";
+}
+
+function closeLoginForm() {
+    let form = document.getElementById("loginForm");
+    form.style.display === "block" ?
+    form.style.display = "none":
+    form.style.display = "block";
+}
+
+function openFormSignup() {
+    let form = document.getElementById("signupForm");
+    form.style.display === "none" ?
+    form.style.display = "block":
+    form.style.display = "none";
+}
+
+function closeSignupForm() {
+    let form = document.getElementById("signupForm");
+    form.style.display === "block" ?
+    form.style.display = "none":
+    form.style.display = "block";
+}
